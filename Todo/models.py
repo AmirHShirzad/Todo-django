89bc4faac1ee506313d2313cae17d96aa4279d86
Amir-H-Shzd <amirshir737@gmail.com>
2022-08-16 23:32:16 +0430

    create a form
    we can now post items and see those in template

--- Todo/models.py
+++ Todo/models.py
@@ -1,3 +1,12 @@
+from re import T
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    complete = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- Todo/views.py
+++ Todo/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from asyncio import tasks
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import HttpResponse, JsonResponse
+from .models import *
+from .forms import *
 
 # Create your views here.
+
+
+def index(request):
+    tasks = Todo.objects.all()
+
+    form = TodoForm()
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'Todo/list.html', context)
+
+
+def update_task(request):
+    return render(request, 'Todo/update_task.html')
